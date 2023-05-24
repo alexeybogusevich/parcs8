520a56e09573e93deefd1e310755f91cf0297112
obohusevych <oleksii.bohusevych@ratesetter.com>
2023-05-24 22:45:56 +0300

    Added tests for the Host project

---
+++ tests/Parcs.Host.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Parcs.Host.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
