93f1118a00c32a5da409ee0b77db7540810278f0
obohusevych <oleksii.bohusevych@ratesetter.com>
2023-05-24 23:05:57 +0300

    Added tests for the Daemon project

---
+++ tests/Parcs.Daemon.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Parcs.Daemon.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
